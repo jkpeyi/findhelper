eab738258f71d4b3a43f27715f99b81cc0c0009d
jonathan <jkpeyi@gmail.com>
2021-10-07 14:32:43 +0100

    first commit

---
+++ src/FindHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+namespace App\Jkpeyi\Findhelper;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class FindHelper
+{
+    public static function findById(Model $model, $id)
+    {
+        try {
+            $obj = $model->findOrFail($id);
+            return $obj;
+        } catch (ModelNotFoundException $e) {
+            abort(422, $e->getModel() . " not found");
+        }
+    }
+}
